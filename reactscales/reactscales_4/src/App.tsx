9d7048ad8e4291d92bbad0713a48fa9ea176090d
sarahbicknell <166096006+sarahbicknell@users.noreply.github.com>
2024-06-15 15:24:06 -0400

    express react scales

--- reactscales/reactscales_4/src/App.tsx
+++ reactscales/reactscales_4/src/App.tsx
@@ -2,32 +2,36 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [input, setInput] = useState(0)
+  const [input1, setInput1] = useState(0)
+  const [input2, setInput2] = useState(0)
   const[grabInput, setGrabInput] = useState(0)
 
-  function handleChange(event) {
-    setInput(event.target.value)
+  function handleChange1(event) {
+    setInput1(event.target.value)
+  }
+
+  function handleChange2(event) {
+    setInput2(event.target.value)
   }
 
   function handleAdd() {
-    let firstNum = Number(input)
-    let secondNum = Number(grabInput)
+    let firstNum = Number(input1)
+    let secondNum = Number(input2)
     setGrabInput(firstNum + secondNum)
   }
 
   function handleSubtract() {
-    let firstNum = Number(input)
-    let secondNum = Number(grabInput)
+    let firstNum = Number(input1)
+    let secondNum = Number(input2)
     setGrabInput(firstNum - secondNum)
   }
 
   return (
     <div className="calc"> 
-      <input type="text" value={input} onChange={handleChange}/> 
+      <input type="text" value={input1} onChange={handleChange1}/> 
+      <input type="text" value={input2} onChange={handleChange2} />
       <button onClick={handleAdd}> + </button>
       <button onClick={handleSubtract}> - </button>
-      <button > * </button>
-      <button > / </button> 
       {grabInput}
     </div> 
   )
